c07ec5a21be861c363366356787d08e4fdd489ad
Jacopo <jacopo.delu@gmail.com>
2016-03-16 20:30:55 +0100

    fixed ethernet header

--- lib/ethernet.h
+++ lib/ethernet.h
@@ -11,6 +11,7 @@
 #define ETHHWASIZE  6       /* Ethernet addr byte len */
 #define MACSTRSIZE  18      /* Mac addr string size */
 #define ETHHDRSIZE  14      /* Ethernet header size */
+#define ETHMINPAYL  64      /* Ethernet min payload */
 #define ETHMAXPAYL  1500    /* Ethernet max payload */
 
 #define ETHTYPE_PUP     0X0200
@@ -19,10 +20,10 @@
 #define ETHTYPE_RARP    0X8035
 
 struct EthHeader {
-    unsigned char dhwaddr[ETHHWASIZE];
-    unsigned char shwaddr[ETHHWASIZE];
-    unsigned int  eth_type:16;
-    unsigned char data[0];
+    unsigned char   dhwaddr[ETHHWASIZE];
+    unsigned char   shwaddr[ETHHWASIZE];
+    unsigned short  eth_type;
+    unsigned char   data[0];
 };
 
 struct EthHeader *build_ethernet_packet(struct sockaddr *src, struct sockaddr *dst, unsigned short type, unsigned long paysize, unsigned char *payload);
