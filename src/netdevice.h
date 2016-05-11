d3756d8c4dc3c5125eb84a02f9f45a136ece2cf1
Jacopo De luca <jacopo.deluca@studenti.unimi.it>
2016-05-11 11:02:53 +0200

    Added partial documentation

--- src/netdevice.h
+++ src/netdevice.h
@@ -14,6 +14,15 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+/**
+ * @file netdevice.h
+ * @brief Create/destroy raw scoket and manage NIC information.
+ *
+ * This file provides generic APIs (Linux/BSD) for create raw socket and manage
+ * network device, you can easily get list of all availlable device and allows you
+ * to modified certain device parameters such as: mac address, NIC flags.
+ */
+
 #ifndef SPARK_NETDEVICE_H
 #define SPARK_NETDEVICE_H
 
@@ -29,20 +38,49 @@
 #define NETD_SUCCESS        1
 #define NETD_NOTSUPPORTED    -1
 
+/**
+ * @brief Contains information about the open socket.
+ */
 struct llOptions {
+	/// @brief Contain interface name, Eg: eno1, wlo1...
     char iface_name[IFNAMSIZ];
+	/// @brief Path of the bound BPF device.	
     char bsd_bind[BPFPATHMAXLEN];
+	/// @brief Socket descriptor.
     int sfd;
+	/// @brief Buffer length for the read operations.
     unsigned long buffl;
 };
 
+/**
+ * @brief Contains device name and pointer to the next structure.
+ */
 struct ifList {
+	/// @brief Device name.
     char name[IFNAMSIZ];
+	/// @brief Next ifList item.
     struct ifList *next;
 };
 
+/**
+ * @brief Get the device burned-in mac address.
+ * @param sd Socket descriptor.
+ * @param iface_name Interface name.
+ * @param hwa Pointer to netaddr_mac structure.
+ * @return Upon successful completion hwa will filled with mac address and funcion returns NETD_SUCCESS.
+ * Otherwise, NETD_UNSUCCESS is returned. If function is not supported NETD_NOTSUPPORTED is returned.
+ * @warning On BSD systems this function returns always NETD_NOTSUPPORTED.
+ */
 int get_burnedin_mac(int sd, char *iface_name, struct netaddr_mac *hwa);
 
+/**
+ * @brief Get the active flag word of the device.
+ * @param sd Socket descriptor.
+ * @param iface_name Interface name.
+ * @param flag Pointer to short int.
+ * @return Upon successful completion flag will filled with active FLGAS and funcion returns NETD_SUCCESS.
+ * Otherwise, NETD_UNSUCCESS is returned.
+ */
 int get_flags(int sd, char *iface_name, short *flag);
 
 int get_hwaddr(int sd, char *iface_name, struct netaddr_mac *hwaddr);
